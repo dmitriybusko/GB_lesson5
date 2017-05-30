c9b385916771ddb3637dcd37f7f8a55749e601be
dmitriybusko <dmitriy.busko@gmail.com>
2017-05-30 13:09:56 +0300

    task with voter

--- script.js
+++ script.js
@@ -40,10 +40,65 @@ function Clock(options){
 	this.stop = stop;
 }
 
+//--------------------------------------------------------------------------------
+
+function Voter(options){
+	var countVote = 0;
+	var elem = document.getElementById(options);
+
+	function render(){
+		var div = document.createElement('div');
+		var down = document.createElement('span');
+		var vote = document.createElement('span');
+		var up = document.createElement('span');
+		div.classList.add('voter');
+		div.id = 'voter';
+		down.classList.add('down');
+		vote.classList.add('vote');
+		up.classList.add('up');
+		down.innerHTML = ' - ';
+		vote.innerHTML = countVote;
+		up.innerHTML = ' + ';
+		div.appendChild(down);
+		div.appendChild(vote);
+		div.appendChild(up);
+		elem.appendChild(div);
+		return div;
+	}
+
+	function setVote(i){
+		countVote = i;
+		view();
+	}
+
+	function main() {
+		var voter = document.getElementById('voter');
+		if(!voter)
+			voter = render();
+		voter.onclick = function(elem) {
+			if(elem.target.className == "up")
+				++countVote;
+			if(elem.target.className == "down")
+				--countVote;
+			view();
+		}
+	}
+
+	function view() {
+		var vote = document.querySelector(".vote");
+		vote.innerHTML = countVote;
+	}
+
+	this.main = main;
+	this.setVote = setVote;
+}
+
 window.onload = function () {
 
 	var clock = new Clock('clock');
 	clock.start();
 	//clock.stop();
-
+	var voter = new Voter('field_voter');
+	voter.main();
+	voter.setVote(4);
 }
